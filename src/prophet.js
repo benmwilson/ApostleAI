176ea75de32fa699edd0c2ec72b7dc2cd170ae7b
Ben Wilson <ben@benmwilson.dev>
2021-04-12 23:02:45 -0700

    implemented twitter api & wikipedia api

--- src/prophet.js
+++ src/prophet.js
@@ -2,9 +2,13 @@
 const logger = require("./logger");
 const config = require("./config");
 const { Wit } = require("node-wit");
-const { json } = require("express");
+const app = require("express");
 const scripture = require("./scripture");
 const porter = require("./porterStemming.js");
+const Twit = require("twit");
+const https = require("https");
+const util = require("util");
+const request = require("request");
 
 // init the wit client using config file
 const client = new Wit({
@@ -46,25 +50,47 @@ Bot.prototype.sendMessage = function (msg) {
           sender: this.id,
           msg: "",
         };
-        
-        if(data.intents[0].name == "wikiQuery"){
-          response.msg = wikiQuery(data.entities["wit$wikipedia_search_query:wikipedia_search_query"][0].value);
-        } else{
-          response.msg = this.pickReply(data, scripture.responses);
-        }
 
-        // temporary console return data with information about the bots response
-        console.log(
-          logger.getTime() +
-            "[Bot with ID " +
-            this.id +
-            "]: sending message " +
-            logger.info(JSON.stringify(response))
-        );
-        
-        // Emit a message event on the socket to be picked up by server
-        this.socket.emit("Intent: ", data.intents[0].name);
-        this.socket.emit("message", response);
+        try {
+          if (data.intents[0].name == "wikiQuery") {
+            //Pass data and bot to function
+            wikiQuery(
+              data.entities[
+                "wit$wikipedia_search_query:wikipedia_search_query"
+              ][0].value,
+              this
+            );
+          } else if (data.intents[0].name == "latestTweet") {
+            //Pass data and bot to function
+            latestTweet(data.entities["wit$contact:contact"][0].value, this);
+          } else {
+            response.msg = this.pickReply(data, scripture.responses);
+            // temporary console return data with information about the bots response
+            console.log(
+              logger.getTime() +
+                "[Bot with ID " +
+                this.id +
+                "]: sending message " +
+                logger.info(JSON.stringify(response))
+            );
+
+            // Emit a message event on the socket to be picked up by server
+            this.socket.emit("message", response);
+          }
+        } catch {
+          response.msg = this.pickReply(data, scripture.responses);
+          // temporary console return data with information about the bots response
+          console.log(
+            logger.getTime() +
+              "[Bot with ID " +
+              this.id +
+              "]: sending message " +
+              logger.info(JSON.stringify(response))
+          );
+
+          // Emit a message event on the socket to be picked up by server
+          this.socket.emit("message", response);
+        }
       })
       // catch errors and log it to console on the error stream
       .catch(logger.error(console.error));
@@ -140,22 +166,101 @@ Bot.prototype.pickReply = function (input, responses) {
   return botReply;
 };
 
-function wikiQuery(query) {
-  // ajax call to pass the bot information from wit ai
-  // $.ajax({
-     url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=prefixsearch&redirects=1&converttitles=1&formatversion=2&exintro=1&explaintext=1&gpssearch=' + query,
-  //   success: function(data){
-  //     // output the witai response to the console
-  //     console.emit("[WIKI RESPONSE]: " + JSON.stringify(data));
-  //     // Save the wit ai response to the console
-  //     //SaveFunction.saveChatLog("\n[WIKI RESPONSE TO] '" + input + "' -> " + JSON.stringify(data) + "\n");
+var T = new Twit({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret,
+});
 
-  //     //send the returned data from wit to the parser here
-  //     return (query);
-  //   }
-  // });
-  loadJSON(url, data);
-  return data;
+function latestTweet(username, bot) {
+  console.log(logger.getTime() + "Querying Twitter for user: " + username);
+  T.get("statuses/user_timeline", { screen_name: username, count: 1 })
+    .then(function (result) {
+      let response = {
+        sender: bot.id,
+        msg: "",
+      };
+
+      if (result.data[0] == null) {
+        console.log(
+          logger.getTime() + logger.error("Could not find user " + username)
+        );
+        response.msg = "Sorry, I couldn't find the user " + username;
+        bot.socket.emit("message", response);
+      } else {
+        response.msg =
+          "Latest tweet from " +
+          result.data[0].user.screen_name +
+          ": " +
+          result.data[0].text;
+        bot.socket.emit("message", response);
+      }
+    })
+    .catch(function () {
+      let response = {
+        sender: bot.id,
+        msg: "",
+      };
+
+      response.msg = "Sorry, I couldn't find the user " + username;
+      console.log(
+        logger.getTime() + logger.error("Could not find user " + username)
+      );
+      bot.socket.emit("message", response);
+    });
+}
+
+function wikiQuery(query, bot) {
+  query = query.slice(0, -1);
+  query = query.replace(" ", "_");
+
+  let response = {
+    sender: bot.id,
+    msg: "",
+  };
+
+  console.log(
+    logger.getTime() +
+      "[Bot with ID " +
+      bot.id +
+      "]: querying Wikipedia for " +
+      query
+  );
+
+  (url =
+    "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=prefixsearch&redirects=1&converttitles=1&formatversion=2&exintro=1&explaintext=1&gpssearch=" +
+    query),
+    https
+      .get(url, (resp) => {
+        let data = "";
+
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        resp.on("end", () => {
+          jsonData = JSON.parse(data).query.pages;
+
+          for (x in jsonData) {
+            if (jsonData[x].index == 1)
+              response.msg = jsonData[x].extract.split(".")[0];
+          }
+
+          console.log(
+            logger.getTime() +
+              logger.info("Wikipedia API Response: ") +
+              response.msg
+          );
+          bot.socket.emit("message", response);
+          response.msg =
+            "Read more: " + "https://en.wikipedia.org/wiki/" + query;
+          bot.socket.emit("message", response);
+        });
+      })
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
 }
 
 module.exports = Bot;
